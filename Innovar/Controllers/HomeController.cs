15be697eee1429bbbea440648c62469dab4e988b
CharlyMora <morandres.carlos@gmail.com>
2021-01-29 17:44:51 -0500

    html 5 detail to index page

--- Innovar/Controllers/HomeController.cs
+++ Innovar/Controllers/HomeController.cs
@@ -26,6 +26,7 @@ namespace Innovar.Controllers
 
         public IActionResult Privacy()
         {
+            //test to change the route
             return View("~/Views/Bordados/Notes.cshtml");
         }
         public IActionResult Notes()
